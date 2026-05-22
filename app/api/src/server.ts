63b5b550fd6dd5e8bf48f23366b67b705add8bc2
guimunizzz <guilherme.muniz5@hotmail.com>
2026-05-21 21:05:22 -0300

    feat: server init

---
+++ app/api/src/server.ts
@@ -0,0 +1,15 @@
+import express from "express";
+import { EnvVar } from "./config/EnvVar";
+import path from 'node:path';
+import router from "./routes/route";
+
+const app = express();
+
+app.use(express.json());
+app.use('/', router)
+
+app.use('/produtos', express.static(path.resolve('uploads/Images')));
+
+app.listen(EnvVar.SERVER_PORT, ()=> {
+    console.log(`Servidor rodando em http://localhost:${EnvVar.SERVER_PORT}`)
+})
